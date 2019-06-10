7246634ee6abbeec6591ed70220d7e995c96b762
baryshevrs <baryshevrs@yandex.ru>
2019-06-10 15:41:18 +0300

    add structure

---
+++ projects/if-viewport-size/src/app/viewport-size/viewport-size.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IfViewportSizeDirective } from './if-viewport-size.directive';
+
+@NgModule({
+  declarations: [IfViewportSizeDirective],
+  imports: [
+    CommonModule
+  ]
+})
+export class ViewportSizeModule { }
